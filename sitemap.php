b1c443f6ebca90a7ecc75b1377aa56a0798419c3
hivee <mgarkusha@mail.ru>
2017-10-04 00:41:43 +0300

    Исправил отображение ссылок в номерах SITEMAP

--- sitemap.php
+++ sitemap.php
@@ -44,7 +44,7 @@ $content .= '
     $priority = $priority - 0.10;
     $catalog_rooms = mysql::select("SELECT id FROM #catalog_rooms WHERE display = '1' ORDER by name");
     foreach ($catalog_rooms as $rooms) {
-        $url = 'http://nsgostdom.com/catalog_rooms/?rooms=' . $rooms['id'];
+        $url = 'http://nsgostdom.com/nomera/?catalog_rooms_id=' . $rooms['id'];
         $content .= '<url>
             <loc>'.$url.'</loc>
             <lastmod>'.$lastBuildDate.'</lastmod>
